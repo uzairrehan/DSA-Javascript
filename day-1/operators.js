f26324e75bff85d084c9c4820519ba6bce51601a
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-02-26 19:01:44 +0500

    operators

--- day-1/operators.js
+++ day-1/operators.js
@@ -2,3 +2,47 @@
 //  + -  *    
 // mod = % ye remainder dega
 // divide = / 
+
+
+
+// relational 
+// > < <= >= != !== == ===
+
+
+// logical
+// && || 
+
+
+// unary
+//  -- ++
+// pre ,post
+
+ 
+
+// errors
+
+// let a = 10++
+
+
+
+// let a = 10
+// let b = ++(a++)
+
+
+
+
+
+
+
+
+console.log(Math.trunc((Math.random()*9000)+1000));
+
+
+
+let a = 5
+let b = 7
+
+a*b
+
+
+
