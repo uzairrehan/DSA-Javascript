4c3717f12448ea08beda894cfd3a03ecd393589c
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-03-01 15:08:37 +0500

    2 question complted

---
+++ day-2/app.js
@@ -0,0 +1,52 @@
+// esa koi bhi code jis jo chalne ke bad true false de woh condition ban sakta he
+// baghair "if" ke "else" syntax error de ga
+
+
+
+
+
+
+
+
+// let ans = Number(prompt("what is yoour age?"));
+// if (isNaN(ans)) {
+//   console.log("Invalid Number");
+// }
+// if (ans >= 18) {
+//   console.log("you can vote");
+// } else {
+//   console.log("you cant vote");
+// }
+
+
+
+
+
+
+
+
+
+// let amount = Number(prompt("what is the final amount"));
+// let dis = 0;
+// if (amount > 0 && amount <= 5000) {
+//   dis = 0;
+// } else if (amount > 5000 && amount <= 7000) {
+//   dis = 5;
+// } else if (amount > 7000 && amount <= 9000) {
+//   dis = 10;
+// } else if (amount > 9000) {
+//   dis = 20;
+// } else {
+//   console.log("wrong input");
+// }
+
+// console.log(amount - Math.floor((5 * amount) / 100));
+
+
+
+
+
+
+
+
+
